8a80239c7c510f71abf0e02ab3fbacd01c7f7c47
devp299 <d.h.vadharodia@gmail.com>
2025-03-20 08:31:25 +0530

    Done Successfully

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,7 +18,7 @@ const Layout = ({ children }: {children: React.ReactNode}) => {
         </section>
         <section className='flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0'>
             <div className='mb-16 lg:hidden'>
-                <Image src="/assets/icons/logo-full-brand.svg" alt='logo' width={224} height={82} className='h-auto w-[200px] lg:w-[250px]' />
+                <Image src="/assets/icons/Group 1.svg" alt='logo' width={224} height={82} className='h-auto w-[200px] lg:w-[250px]' />
             </div>
             { children }
         </section>
